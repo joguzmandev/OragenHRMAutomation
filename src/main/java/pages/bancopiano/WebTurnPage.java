e548eae8e4c5019f355465386012d7861774e2c4
JoGuzmanDev <joguzmandev@gmail.com>
2021-08-19 09:45:22 -0400

    Create Web Turn Page Object Model

---
+++ src/main/java/pages/bancopiano/WebTurnPage.java
@@ -0,0 +1,12 @@
+package pages.bancopiano;
+import org.openqa.selenium.WebDriver;
+
+public class WebTurnPage {
+	
+	private WebDriver webDriver;
+	
+	public WebTurnPage(WebDriver webDriver) {
+		this.webDriver = webDriver;
+	}
+
+}
